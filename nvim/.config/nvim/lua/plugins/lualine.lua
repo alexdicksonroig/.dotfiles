4d5816679e5031eec7da2891ec867bce81fca64d
Alex Dickson 2 <alexdickson2@MacBook-Pro-de-infraestructura-9.local>
2024-08-29 22:18:39 +0200

    Added lualine back

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            options = {
+                theme = 'tokyonight',
+                section_separators = { '', '' },
+                component_separators = { '', '' },
+            },
+            sections = {
+                lualine_a = {
+                    { 'mode', fmt = function(str) return str:sub(1,1) end } }, 
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            extensions = { 'nvim-tree' },
+        }
+    end,
+}
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,6 +3,13 @@ return {
 	priority = 1000,
 	init = function()
 		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+  highlight SignColumn guibg=none
+]]
 		vim.cmd.hi("Comment gui=none")
 	end,
 }
