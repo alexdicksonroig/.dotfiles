7eb47ea2d0651daaa5215e469669e5da389c580c
Alex@Arch <alexdickson53@gmail.com>
2024-09-02 18:30:07 +0200

    Lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,8 +9,7 @@ return {
                 component_separators = { '', '' },
             },
             sections = {
-                lualine_a = {
-                    { 'mode', fmt = function(str) return str:sub(1,1) end } }, 
+                lualine_a = { 'mode' }, 
                 lualine_b = { 'branch' },
                 lualine_c = { 'filename' },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
