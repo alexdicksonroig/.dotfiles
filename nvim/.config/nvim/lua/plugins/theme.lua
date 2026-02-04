0f32ab37d636a3ddc1db4e3e352fa51a13c45ee2
Alex <alex.dickson@immfly.com>
2026-02-04 21:43:20 +0100

    New working config

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -13,7 +13,7 @@ return { -- You can easily change to a different colorscheme.
 	"folke/tokyonight.nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	init = function()
-		vim.cmd.colorscheme("tokyonight")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
 --]]
